a85d7c4764f660d41869b1aa09b59d2b84ed7a7e
hojmark <1203136+hojmark@users.noreply.github.com>
2026-02-07 13:06:24 +0100

    chore: .net bug workaround

--- dotnet/Containers/Tag.cs
+++ dotnet/Containers/Tag.cs
@@ -25,7 +25,10 @@ public record LatestTag : Tag {
 }
 
 public static class TagExtensions {
+  // Bug: https://github.com/dotnet/sdk/issues/51681
+#pragma warning disable CA1034
   extension( Tag ) {
+#pragma warning restore CA1034
     public static Tag Latest => LatestTag.Instance;
     public static Tag Dev => new("dev");
 
