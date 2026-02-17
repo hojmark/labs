cd789c90f59ea25e255fc2585ec6a7ed856543dd
hojmark <1203136+hojmark@users.noreply.github.com>
2026-02-17 22:50:29 +0100

    add dockerbuildsettingsextensions

---
+++ dotnet/ImageReferences.Extensions.Nuke/DockerBuildSettingsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Nuke.Common.Tools.Docker;
+
+namespace HLabs.ImageReferences.Extensions.Nuke;
+
+/// <summary>
+/// Extension methods for <see cref="DockerBuildSettings"/> to work with strongly-typed image references.
+/// </summary>
+public static class DockerBuildSettingsExtensions {
+  /// <summary>
+  /// Sets the image tag using a <see cref="QualifiedImageRef"/>.
+  /// </summary>
+  /// <param name="settings">The Docker build settings.</param>
+  /// <param name="imageReference">The image reference to tag.</param>
+  /// <returns>The updated settings.</returns>
+  public static DockerBuildSettings SetTag( this DockerBuildSettings settings, QualifiedImageRef imageReference ) {
+    ArgumentNullException.ThrowIfNull( imageReference );
+    return global::Nuke.Common.Tools.Docker.DockerBuildSettingsExtensions.SetTag( settings, imageReference.ToString() );
+  }
+}
